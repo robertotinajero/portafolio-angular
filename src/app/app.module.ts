667931eda63ed44d6041ad66996b2ce6118113c0
Roberto Tinajero <robertofast92@live.com.mx>
2018-08-21 12:58:56 -0500

    Navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule} from './app.routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PortafolioComponent } from './components/pages/portafolio/portafolio.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ItemComponent } from './components/pages/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
